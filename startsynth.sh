150c7cdf99e542f8f50bc7e654b2898537da2379
Pete Dring <dringp@fulford.york.sch.uk>
2022-03-19 16:25:51 +0000

    Update startsynth.sh

--- startsynth.sh
+++ startsynth.sh
@@ -1,4 +1,4 @@
 sudo killall fluidsynth
 killall python3
-sudo fluidsynth  -si -r44100 -c8 -z64 --audio-driver=alsa --gain 1 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
+sudo fluidsynth  -si -r44100 -c3 -z64 --audio-driver=alsa --gain 1 /usr/share/sounds/sf2/FluidR3_GM.sf2 &
 python3 main.py &
