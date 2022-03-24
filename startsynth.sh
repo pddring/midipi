296d1f8ca85e17b5366c813ed8e3696c45752c19
Pete Dring <dringp@fulford.york.sch.uk>
2022-03-24 13:51:04 +0000

    Update startsynth.sh

--- startsynth.sh
+++ startsynth.sh
@@ -1,4 +1,4 @@
 sudo killall fluidsynth
 killall python3
-sudo fluidsynth  -si -r44100 -c3 -z64 --audio-driver=alsa --gain 1 /usr/share/sounds/sf2/FluidR3_GM.sf2 -o alsa.device=plughw:IQaudIODAC &
+sudo fluidsynth  -si -r44100 -c3 -z64 --audio-driver=alsa --gain 1 /usr/share/sounds/sf2/FluidR3_GM.sf2 -o audio.alsa.device=plughw:IQaudIODAC &
 python3 main.py &
